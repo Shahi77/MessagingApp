f32d05405baf77309901612facf7f34b462355dd
Shahi77 <shahiinfinity02@gmail.com>
2024-10-17 23:23:55 +0530

    fix in user schema

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -15,10 +15,6 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
   },
   { timestamps: true }
 );
