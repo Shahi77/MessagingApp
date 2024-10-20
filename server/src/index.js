711e488bdbfd20d0a896851bf9a56d68e52b03c6
Shahi77 <shahiinfinity02@gmail.com>
2024-10-20 21:01:45 +0530

    adding morgan

--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,14 @@ const cookieParser = require("cookie-parser");
 const { app } = require("./service/socketServer");
 const { connectDB } = require("./service/dbConnect");
 const v1Router = require("./routes/version1.routes");
+const morgan = require("morgan");
 require("dotenv").config();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(morgan("dev"));
 
 app.use("/v1", v1Router);
 const init = async () => {
